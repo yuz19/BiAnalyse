79de010cc08b0d712cc48b5e2c178ae5b8a1c56f
nabil19 <funity111@gmail.com>
2024-04-18 19:33:23 +0100

    changeCausalite

--- CalculeCauslite.py
+++ CalculeCauslite.py
@@ -65,11 +65,24 @@ class CalculeCauslite:
         if E1_cause.Measure == E2_effect.Measure:
             self.DI = 0
         else:
-            E1_pos_dates = sorted(E1_cause.pos_dates, key=lambda x: datetime.strptime(x, '%Y-%m-%d'), reverse=True)
+            if datetime.strptime(E1_cause.pos_dates[0], '%Y-%m-%d'):
+                E1_pos_dates = sorted(E1_cause.pos_dates, key=lambda x: datetime.strptime(x, '%Y-%m-%d'), reverse=True)
 
+                
+                E2_pos_dates = sorted(E2_effect.pos_dates, key=lambda x: datetime.strptime(x, '%Y-%m-%d'), reverse=True)
+            elif datetime.strptime(E1_cause.pos_dates[0], '%Y-%m'):
+                E1_pos_dates = sorted(E1_cause.pos_dates, key=lambda x: datetime.strptime(x, '%Y-%m'), reverse=True)
+
+                
+                E2_pos_dates = sorted(E2_effect.pos_dates, key=lambda x: datetime.strptime(x, '%Y-%m'), reverse=True)
+            elif  datetime.strptime(E1_cause.pos_dates[0], '%Y'):
+                E1_pos_dates = sorted(E1_cause.pos_dates, key=lambda x: datetime.strptime(x, '%Y'), reverse=True)
+
+                
+                E2_pos_dates = sorted(E2_effect.pos_dates, key=lambda x: datetime.strptime(x, '%Y'), reverse=True)
+                
             E1_pos_dates = [datetime.strptime(date_str, '%Y-%m-%d') for date_str in E1_pos_dates]
             
-            E2_pos_dates = sorted(E2_effect.pos_dates, key=lambda x: datetime.strptime(x, '%Y-%m-%d'), reverse=True)
             E2_pos_dates = [datetime.strptime(date_str, '%Y-%m-%d') for date_str in E2_pos_dates]
             print(E2_effect.RefEvent,"after",E2_pos_dates)
             end1 = len(E2_pos_dates)                
